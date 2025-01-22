859b64440806762e93f073e0f24d307cd5760c7e
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2025-01-22 07:37:59 +0100

    Update example

--- src/main/java/com/example/azure_key_vault_native/AzureKeyVaultNativeApplication.java
+++ src/main/java/com/example/azure_key_vault_native/AzureKeyVaultNativeApplication.java
@@ -1,8 +1,14 @@
 package com.example.azure_key_vault_native;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.spring.cloud.autoconfigure.implementation.context.properties.AzureGlobalProperties;
+import com.azure.spring.cloud.core.customizer.AzureServiceClientBuilderCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -15,5 +21,19 @@ public class AzureKeyVaultNativeApplication {
     @Bean
     public AzureGlobalProperties azureGlobalProperties() {
         return new AzureGlobalProperties();
+    }
+
+    @Bean
+    public AzureServiceClientBuilderCustomizer<SecretClientBuilder> secretClientBuilderCustomizer() {
+        return builder -> builder.credential(new DefaultAzureCredentialBuilder().build());
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationStartedEvent> applicationInitializers(SecretClient secretClient) {
+        return event -> {
+            System.out.println("Azure Key Vault Native Application Started");
+            String password = secretClient.getSecret("some-secret").getValue();
+            System.out.println(password);
+        };
     }
 }
