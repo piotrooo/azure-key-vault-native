241974b6b717b95788f2a3476f7cdbd3f57b5d30
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2025-03-03 08:34:49 +0100

    Update

--- src/main/java/com/example/azure_key_vault_native/AzureKeyVaultNativeApplication.java
+++ src/main/java/com/example/azure_key_vault_native/AzureKeyVaultNativeApplication.java
@@ -3,8 +3,11 @@ package com.example.azure_key_vault_native;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.implementation.models.SecretAttributes;
+import com.azure.security.keyvault.secrets.implementation.models.SecretBundle;
 import com.azure.spring.cloud.autoconfigure.implementation.context.properties.AzureGlobalProperties;
 import com.azure.spring.cloud.core.customizer.AzureServiceClientBuilderCustomizer;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -12,8 +15,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RegisterReflectionForBinding({SecretBundle.class, SecretAttributes.class})
 public class AzureKeyVaultNativeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AzureKeyVaultNativeApplication.class, args);
     }
